286cdd6ab4e32a0b5609ee821b4b021933dcc786
ItzArima <skaypaprilia@gmail.com>
2021-10-15 16:02:19 +0200

    array completati senza active

--- main.js
+++ main.js
@@ -98,3 +98,67 @@ function imgdown(){
     }
     console.log(active);
 }
+
+
+const items = [
+    'img/01.jpg', //0
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'  //4
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+];
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+];
+let i=0;
+
+function imgdownarray()
+{
+    if (i > (items.length-2)){
+        i=0;
+    }
+    else {
+        i = i+1;
+    }    
+    var img = document.getElementById("img-display");
+    img.src = items[i]; 
+    var titolo = document.getElementById("img-title");
+    titolo.innerHTML = title[i];
+    var testo  = document.getElementById("img-text")
+    testo.innerHTML = text[i];
+    console.log(i);
+    console.log(title[i]);
+    console.log(text[i]);
+}
+
+function imguparray()
+{
+    if (i == 0 ){
+        i=4;
+    }
+    else {
+        i = i-1;
+    }    
+    var img = document.getElementById("img-display");
+    img.src = items[i]; 
+    var titolo = document.getElementById("img-title");
+    titolo.innerHTML = title[i];
+    var testo  = document.getElementById("img-text")
+    testo.innerHTML = text[i];
+    console.log(i);
+    console.log(title[i]);
+    console.log(text[i]);
+}
